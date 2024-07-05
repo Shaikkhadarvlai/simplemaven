acce454bd0070429ebe18e4ae5b9112fe6705321
Shaikkhadarvlai <khadarsk414@gmail.com>
2024-07-05 16:19:33 +0530

    this is maven commit

---
+++ src/test/java/testPackage/Firstcode.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class Firstcode
+{
+	public static void main(String[]ars)
+	{
+		System.out.println("Hello all friends");
+		
+	}
+	
+
+	
+}
---
+++ src/test/java/testPackage/Scecondcode.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class Scecondcode 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi my name is khadar vali");
+
+	}
+
+}
